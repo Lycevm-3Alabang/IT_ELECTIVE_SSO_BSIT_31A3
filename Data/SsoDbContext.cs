0eca947433ff4f4fdda6089cae85fbff889a36fd
ninalamo <nin.alamo@outlook.com>
2026-08-29 13:34:46 +0800

    add dbcontext

---
+++ Data/SsoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class SsoDbContext : IdentityDbContext<IdentityUser>
+{
+    public SsoDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected SsoDbContext()
+    {
+    }
+}
