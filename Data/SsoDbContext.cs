ab94ce36b264cb43adc133391277337b1aa4232f
Yuwan Andrei Gregorio <yuwanandrei@gmail.com>
2026-08-30 16:06:45 +0800

    Define UserGroup junction entity

--- Data/SsoDbContext.cs
+++ Data/SsoDbContext.cs
@@ -15,19 +15,54 @@ public class SsoDbContext : IdentityDbContext<IdentityUser>
     {
     }
 
+    public DbSet<TenantApp> TenantApps => Set<TenantApp>();
+
+    public DbSet<Group> Groups => Set<Group>();
+
+    public DbSet<UserGroup> UserGroups => Set<UserGroup>();
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
         builder.Entity<TenantApp>(t =>
         {
-            t.Property(a => a.Name)
-            .IsRequired()
-            .HasColumnName("Name")
-            .HasMaxLength(100);
-
-
             t.HasKey(a => a.Id);
+
+            t.Property(a => a.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasMaxLength(100);
+        });
+
+        builder.Entity<Group>(g =>
+        {
+            g.HasKey(x => x.Id);
+
+            g.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasMaxLength(100);
+
+            g.HasOne(x => x.TenantApp)
+                .WithMany(t => t.Groups)
+                .HasForeignKey(x => x.TenantAppId)
+                .OnDelete(DeleteBehavior.Cascade); 
+        });
+
+        builder.Entity<UserGroup>(ug =>
+        {
+            ug.HasKey(x => new { x.UserId, x.GroupId });
+
+            ug.HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade); 
+
+            ug.HasOne(x => x.Group)
+                .WithMany()
+                .HasForeignKey(x => x.GroupId)
+                .OnDelete(DeleteBehavior.Cascade);
         });
     }
 }
---
+++ Models/userGroup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Models;
+
+public class UserGroup
+{
+
+    public string UserId { get; set; } = string.Empty;
+    public IdentityUser User { get; set; } = null!;
+
+    public int GroupId { get; set; }
+    public Group Group { get; set; } = null!;
+}
