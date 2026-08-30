5dcbb7f7d3ed4737bd97fa7b9ca25c8871194139
Yuwan Andrei Gregorio <yuwanandrei@gmail.com>
2026-08-30 14:35:02 +0800

    Define Group entity with FK to TenantApp

---
+++ Models/Group.cs
@@ -0,0 +1,12 @@
+﻿namespace Models;
+
+public class Group
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public int TenantAppId { get; set; }
+
+    public TenantApp TenantApp { get; set; } = null!;
+}
