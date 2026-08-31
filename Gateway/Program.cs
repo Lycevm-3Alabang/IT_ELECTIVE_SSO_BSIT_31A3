d7c57a1270c2f268a4e4bb23ad26c31704c6f2e4
Yuwan Andrei Gregorio <yuwanandrei@gmail.com>
2026-08-31 12:42:30 +0800

    rs-OnModelCreating

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,7 +1,13 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SsoDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
