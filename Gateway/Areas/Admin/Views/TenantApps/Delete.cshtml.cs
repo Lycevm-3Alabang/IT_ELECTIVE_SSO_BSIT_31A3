aa5a96f8739e6ce56445b54f6c178749ab87fe77
ishx <pelarcaharveyischei@gmail.com>
2026-09-26 15:01:55 +0800

    fix 12 15

---
+++ Gateway/Areas/Admin/Views/TenantApps/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gateway.Areas.Admin.Views.TenantApps
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
